279d6743da3528d637df2c8aab3a2f7470bf0c98
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-05-02 03:41:52 +0900

    [modify] Update api.py

--- pygrf/api.py
+++ pygrf/api.py
@@ -1,8 +1,45 @@
+from io import BufferedReader
 from .exceptions import GRFParseError
-from . import gat, grf_v2, grf_v3, spr, act
+from . import gat, grf_v1, grf_v2, grf_v3, spr, act
 
 
-def open_grf(fp):
+def open_grf(fp: BufferedReader) -> any:
+    """
+    Open a GRF archive
+
+    :param filename: the path to the grf archive file
+    """
+    grf = None
+    try:
+        grf = grf_v3.GRF(fp)
+    except GRFParseError:
+        try:
+            # If the file is not a GRF v3, try to parse it as GRF v2
+            fp.seek(0)
+            grf = grf_v2.GRF(fp)
+        except GRFParseError:
+            try:
+                # If the file is not a GRF v2, try to parse it as GRF v1
+                fp.seek(0)
+                grf = grf_v1.GRF(fp)
+            except GRFParseError as ex:
+                raise GRFParseError(f"Failed to parse GRF file: {ex}")
+    return grf
+
+def open_grf_v1(fp: BufferedReader) -> grf_v1.GRF|None:
+    """
+    Open a GRF archive
+
+    :param filename: the path to the grf archive file
+    """
+    grf = None
+    try:
+        grf = grf_v1.GRF(fp)
+    except GRFParseError as ex:
+        raise GRFParseError(f"Failed to parse GRF file: {ex}")
+    return grf
+
+def open_grf_v2(fp: BufferedReader) -> grf_v2.GRF|None:
     """
     Open a GRF archive
 
@@ -11,15 +48,22 @@ def open_grf(fp):
     grf = None
     try:
         grf = grf_v2.GRF(fp)
-    except GRFParseError as ex1:
-        try:
-            # If the file is not a GRF v2, try to parse it as GRF v3
-            fp.seek(0)
-            grf = grf_v3.GRF(fp)
-        except GRFParseError as ex2:
-            raise GRFParseError(f"Failed to parse GRF file: {ex2}")
+    except GRFParseError as ex:
+        raise GRFParseError(f"Failed to parse GRF file: {ex}")
     return grf
 
+def open_grf_v3(fp: BufferedReader) -> grf_v3.GRF|None:
+    """
+    Open a GRF archive
+
+    :param filename: the path to the grf archive file
+    """
+    grf = None
+    try:
+        grf = grf_v3.GRF(fp)
+    except GRFParseError as ex:
+        raise GRFParseError(f"Failed to parse GRF file: {ex}")
+    return grf
 
 def open_gat(filename: str) -> gat.GAT:
     """
